860f18e92872d9e3f085b6f3dd9fd201ee81082b
Sid-Lais <sidlais.ls@gmail.com>
2025-03-31 16:07:02 +0530

    Delete unecessary files

--- sso-python/frontend/src/main.jsx
+++ sso-python/frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './styles/Login.css' // Import the main CSS file
+import './styles/Login.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
